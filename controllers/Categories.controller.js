7ae3b5869c872bab6d3d3da9a0248545307e369d
Marco <marquinco@outlook.com>
2022-03-17 20:24:53 -0300

    Fix: Corrige variavel com nome teste no endpoint post /categories

--- controllers/Categories.controller.js
+++ controllers/Categories.controller.js
@@ -4,10 +4,18 @@ const createCategory = async (req, res, next) => {
   try {
     const { name } = req.body;
     if (!name) return res.status(400).json({ message: '"name" is required' });
-    const teste = await Category.create({ name });
-    return res.status(201).json(teste);
+    const category = await Category.create({ name });
+    return res.status(201).json(category);
   } catch (error) {
     next(error);
+  }
+};
+
+const getCategories = async (req, res, next) => {
+  try {
+    const
+  } catch (error) {
+    
   }
 };
 module.exports = {
