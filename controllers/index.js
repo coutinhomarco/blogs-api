1dc1b1170fbc77cf31486d632a0831f17851d04c
Marco <marquinco@outlook.com>
2022-03-17 20:22:42 -0300

    Fix: Corrige organizacao pasta controllers e importacao do controller do post Categories

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const { login, createUser,
   getUsers, getOne } = require('./User.controller.js');
+const { createCategory } = require('./Categories.controller.js');
 
 module.exports = { 
   login,
   createUser,
   getUsers,
   getOne,
+  createCategory,
 };
